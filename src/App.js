65d862c04596b2d68ff189e033c00719781d478e
Sifflion <dami.m.gonza@gmail.com>
2022-03-22 18:06:18 -0300

    rwr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import { Routes, Route} from "react-router-dom";
+import Layout from "./Layout";
+import Seccion2 from "./seccion2";
+import Seccion3 from "./seccion3";
+import Seccion4 from "./seccion4";
+import Seccion1 from "./seccion1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+             <Routes>
+                <Route exact path="/" element={<Layout />}>
+                <Route exact path="seccion1" element={<Seccion1 />}/>
+                <Route exact path="seccion2" element ={<Seccion2 />}/>
+                <Route exact path="seccion3" element ={<Seccion3 />}/>
+                <Route exact path="seccion4" element={<Seccion4 />}/>
+                </Route>                         
+             </Routes>                 
+    );
 }
-
-export default App;
